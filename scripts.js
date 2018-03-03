7d73c4939c97ebb8de2389c0bdfd51b7cf463178
Amir Patel <amirpatel89@gmail.com>
2018-03-03 11:52:24 -0500

    fix geolocation

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function getWhere(){
       navigator.geolocation.getCurrentPosition(function(position) {
       // console.log(`lat: ${position.coords.latitude} lng: ${position.coords.longitude}`);
       var thisLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
-      initMap(thisLocation);  
+      console.log(thisLocation);
   },
   );
 }
