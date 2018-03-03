4529649bc0b87e5ec1db703bcbc94230c5184697
Amir Patel <amirpatel89@gmail.com>
2018-03-03 14:45:46 -0500

    add map with marker

--- scripts.js
+++ scripts.js
@@ -1,8 +1,37 @@
+var globalThisLocation = [];
+var map;
+var infowindow;
+var restArray = [];
+var sorted;
+var defaultlocation = {lat: 33.7831032, lng: -84.3767911}
+
+
+
+
 function getWhere(){
       navigator.geolocation.getCurrentPosition(function(position) {
-      // console.log(`lat: ${position.coords.latitude} lng: ${position.coords.longitude}`);
-      var thisLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
-      console.log(thisLocation);
-  },
-  );
+        // console.log(`lat: ${position.coords.latitude} lng: ${position.coords.longitude}`);
+        var thisLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
+        console.log(thisLocation);
+        console.log(defaultlocation)
+        initMap(thisLocation);
+        globalThisLocation.push(thisLocation);
+      },);
+}
+function initMap(location) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: location,
+    zoom: 14
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: 'Hello World!'
+  });
+  var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+  var parkMarker = new google.maps.Marker({
+    position: defaultlocation,
+    map: map,
+    icon: image
+  });
 }
