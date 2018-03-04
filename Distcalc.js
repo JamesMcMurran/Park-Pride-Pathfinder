b76e90b11373a22481aa4b0dcbb450058fcbf49f
james <james@mcmurran.org>
2018-03-04 12:09:16 -0500

    cleaned the distcalc up some.

--- Distcalc.js
+++ Distcalc.js
@@ -1,13 +1,12 @@
 
 function distance (x1,y1,x2 ,y2) {
-    R = 3959; // earth's mean radius in miles
-    dLat  = deg2rad(x2 - x1);
-    dLong = deg2rad(y2 - y1);
-
-    a =  Math.sin(dLat/2) *  Math.sin(dLat/2) +
+   var R = 3959; // earth's mean radius in miles
+   var dLat  = deg2rad(x2 - x1);
+   var dLong = deg2rad(y2 - y1);
+   var a =  Math.sin(dLat/2) *  Math.sin(dLat/2) +
         Math.cos(deg2rad(x1)) *  Math.cos(deg2rad(x2)) *  Math.sin(dLong/2) *  Math.sin(dLong/2);
-    c = 2 *  Math.atan2( Math.sqrt(a),  Math.sqrt(1-a));
-    d = R * c;
+   var c = 2 *  Math.atan2( Math.sqrt(a),  Math.sqrt(1-a));
+   var d = R * c;
     return d; //distance in miles
 }
 
@@ -15,6 +14,13 @@ function deg2rad(degrees) {
     return degrees * Math.PI / 180;
 }
 
+/**
+ *
+ * @param Obj the Obj of the items that need to be sorted
+ * @param lat
+ * @param long
+ * @returns {Array.<T>|string|Blob|ArrayBuffer}
+ */
 function sortObjByDistance(Obj,lat,long) {
     var tempObj = Obj.slice(0);
     tempObj.sort(function(a,b) {
@@ -23,8 +29,5 @@ function sortObjByDistance(Obj,lat,long) {
     return tempObj;
 }
 
-
-sortedObj = sortObjByDistance(test.parks,33.6746282, -84.4970713);
-
-console.log(sortedObj);
+console.log(sortObjByDistance(test.parks,33.6746282, -84.4970713));
 
